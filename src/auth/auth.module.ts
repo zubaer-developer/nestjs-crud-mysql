bb1af44cb5e9ae48d416b25e24aaddb93181c5d3
Zubaer Hossain <zubaer.developer@gmail.com>
2025-06-26 23:13:09 +0600

    login api created with JWT Token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,27 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { MailModule } from 'src/mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { 
+          expiresIn: configService.get('JWT_EXPIRATION')
+        },
+      }),
+      inject: [ConfigService],
+    }),
     MailModule
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
